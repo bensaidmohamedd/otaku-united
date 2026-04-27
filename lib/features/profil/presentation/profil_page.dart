fc310ac6a4391c665e1190ae6c1c76445bb044c4
Ben Said <bensaidm247@gmail.com>
2026-04-27 23:05:17 +0100

    création de la page de profile

--- lib/features/profil/presentation/profil_page.dart
+++ lib/features/profil/presentation/profil_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:manga/core/theme.dart';
+import 'package:manga/features/watchlists/presentation/page/watchlist_page.dart';
 
 class ProfilPage extends StatelessWidget {
   const ProfilPage({super.key});
@@ -7,16 +7,107 @@ class ProfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profil'),
-        backgroundColor: AppTheme.darkTheme.appBarTheme.backgroundColor,
-      ),
-      body: const Center(
-        child: Text(
-          'Page de Profil',
-          style: TextStyle(fontSize: 24),
+      appBar: AppBar(title: const Text("Profil"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // Avatar + Nom + Email
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/ben.jpg"),
+            ),
+            const SizedBox(height: 10),
+
+            const Text(
+              "Ben Said",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            const Text(
+              "bensaid@email.com",
+              style: TextStyle(color: Colors.grey),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Stats
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [_StatItem(title: "Watchlist", value: "07")],
+            ),
+
+            const SizedBox(height: 30),
+
+            //Boutons
+            _buildButton(
+              icon: Icons.bookmark,
+              color: Colors.grey,
+
+              text: "Ma Watchlist",
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const WatchlistPage()),
+                );
+              },
+            ),
+
+            _buildButton(
+              icon: Icons.settings,
+              color: Colors.grey,
+              text: "Paramètres",
+              onTap: () {},
+            ),
+
+            _buildButton(
+              icon: Icons.logout,
+              text: "Déconnexion",
+              color: Colors.red,
+              onTap: () {},
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  //Widget bouton
+  Widget _buildButton({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+    Color color = Colors.black,
+  }) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: Icon(icon, color: color),
+        title: Text(text),
+        trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+        onTap: onTap,
+      ),
+    );
+  }
+}
+
+//Widget Stat
+class _StatItem extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const _StatItem({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        Text(title),
+      ],
     );
   }
 }
